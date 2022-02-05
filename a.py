fb573b9bcaaae1bf8ebe44091c87a6cc40dedd60
Raunak Mantri <mantrir20@gmail.com>
2022-02-06 03:40:56 +0530

    Final Changes

--- a.py
+++ a.py
@@ -66,7 +66,7 @@ td {{
 </body>
 </HTML>"""
 
-items = [['1', 'Addition and Subtraction', '1', 'Raunak, Mohit', '<a href=https://docs.google.com/document/d/1OOiFWetpGMXIExZqT6TCajRONpbPpZZ2khyjA8PXIXc/edit?usp=sharing>Link</a>'], 
+items = [['1', 'Measurement', '2', 'Aarya, Raunak', '<a href=https://docs.google.com/document/d/10UtvxtnTahkMEtaWQ_9nx8UH59OJx1tT4KxNeJv5GmY/edit#heading=h.enexohuphl1j>Link</a>'], 
     ['2', 'Addition and Subtraction', '1', 'Raunak, Mohit', '<a href=https://docs.google.com/document/d/1OOiFWetpGMXIExZqT6TCajRONpbPpZZ2khyjA8PXIXc/edit?usp=sharing>Link</a>'],
     ['3', 'Addition and Subtraction', '1', 'Raunak, Mohit', '<a href=https://docs.google.com/document/d/1OOiFWetpGMXIExZqT6TCajRONpbPpZZ2khyjA8PXIXc/edit?usp=sharing>Link</a>']
     ]
